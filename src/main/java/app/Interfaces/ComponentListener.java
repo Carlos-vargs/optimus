573bf718efe3161db1ff2c91ed0871540802afce
Carlos-vargs <cvargaslopez769@gmail.com>
2023-07-13 11:44:16 -0600

    ci: add interfaces

---
+++ src/main/java/app/Interfaces/ComponentListener.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package app.Interfaces;
+
+import resources.views.CreateComponentPanel;
+
+/**
+ *
+ * @author carlosvargas
+ */
+public interface ComponentListener {
+
+    void onComponentDeleteRequested(CreateComponentPanel panel);
+}
---
+++ src/main/java/app/Interfaces/EventDateChooser.java
@@ -0,0 +1,8 @@
+package app.Interfaces;
+
+import resources.components.datechooser.SelectedDate;
+
+public interface EventDateChooser {
+
+    public void dateSelected(SelectedAction action, SelectedDate date);
+}
